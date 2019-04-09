0a2cbbe72ba27e11d9d2d642885e230ff7191f34
Sally Yik <sallyyik@hotmail.com>
2019-04-09 13:17:37 +1000

    update git ignore

--- OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -8,13 +8,17 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
+  
         // GET: Cuisine
 
         // Actions are just public methods inside a controller class
         public ActionResult Search(string name)
         {
             var message = Server.HtmlEncode(name);
-            return Content(message);
+            //return Content(message);
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About" }); // redirect to an action (index action, home controller)
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            return Json(new { Message = message, Name = "Scott" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
